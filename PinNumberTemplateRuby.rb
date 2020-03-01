b1863786f8eb133beeb3c7dc73cbe8aafa6414c7
Mikołaj Strusiński <mikistrus@gmail.com>
2020-03-01 07:58:40 +0100

    Minor comment changes

--- PinNumberTemplateRuby.rb
+++ PinNumberTemplateRuby.rb
@@ -1,9 +1,12 @@
-pin = "1234"
-counter = 3
+pin = "1234"  #this variable needs to be a string in order to make it possible to enter 
+			  #whatever pin you like
+counter = 3	  #this counter counts the attempts, and loggs out the user if he attempts to log in more 
+			  #than three times
 loop do
 	counter-=1
 	puts "Enter PIN number: "
-	user_input = gets.chomp
+	user_input = gets.chomp  #if the user input is a string and its compared to a string (pin) then 
+							 #there will be no errors 
 	if user_input == pin
 		system("color 2")
 		puts "Correct Pin."
@@ -22,3 +25,6 @@ loop do
 	end
  break if counter == 0
 end
+
+#built in Sublime Text 3
+#tested in cmd
